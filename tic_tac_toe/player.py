7350abf865a20ea0a4260f9ccfdf19ab05066875
gloria <gloriacheung812@gmail.com>
2022-11-05 14:50:32 -0400

    define getmove for computer to randomly choose index of board and for human to choose index from 0-9

--- tic_tac_toe/player.py
+++ tic_tac_toe/player.py
@@ -14,7 +14,8 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        square = random.choice(game.available_moves())
+        return square
 
 
 class HumanPlayer(Player):
@@ -22,4 +23,16 @@ class HumanPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(f"{self.letter}'s turn. Input move(0-9): ")
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+
+            except ValueError:
+                print("Invalid square, try again")
+        return val
