e03529239ee0e6428a4d2095b36b29a842970bbd
gloria <gloriacheung812@gmail.com>
2022-11-08 14:58:35 -0500

    use beautful soup to scrape list of articles from website and find most upvoted article with link

--- web_scraping/ycnews.py
+++ web_scraping/ycnews.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 
+# using beautiful soup to scrape data from news website to find most upvoted article from list of top 30
 response = requests.get("https://news.ycombinator.com/news")
 yc_webpage = response.text
 soup = BeautifulSoup(yc_webpage, "html.parser")
@@ -17,37 +18,12 @@ for article in articles:
     article_texts.append(article.find("a").getText())
     article_links.append(article.find("a").get("href"))
 
-print(article_texts)
-print(article_links)
-print(article_upvotes)
+# print(article_texts)
+# print(article_links)
+# print(article_upvotes)
 
 max_upvote = max(article_upvotes)
 article_idx = article_upvotes.index(max_upvote)
 most_upvoted_article_text = article_texts[article_idx]
 most_upvoted_article_link = article_links[article_idx]
 print(f"The most upvoted article is: {most_upvoted_article_text} which can be found at {most_upvoted_article_link}")
-
-# with open("website.html") as file:
-#     contents = file.read()
-#
-# soup = BeautifulSoup(contents, "html.parser")
-# print(soup.title)
-# # print(soup.title.string)
-# # print(soup.prettify())
-# tags = soup.find_all(name="a")
-# for tag in tags:
-#     print(tag.get("href"))
-#
-# heading = soup.find(name="h1", id="name")
-# print(heading.text)
-#
-# section_heading = soup.find(name="h3", class_="heading")
-# print(section_heading)
-#
-# company_url = soup.select_one(selector="p a") # can removve selector
-# name = soup.select_one("#name")
-# print(company_url)
-# print(name)
-#
-# headings = soup.select(".heading")
-# print(headings)
