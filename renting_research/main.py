458e3f6ebafd211984c496414a32cefacd76f2e3
gloria <gloriacheung812@gmail.com>
2022-11-09 16:05:23 -0500

    initial commit for project

---
+++ renting_research/main.py
@@ -0,0 +1,6 @@
+# https://forms.gle/avLBSnqfbQDU2fpD7
+# https://www.zillow.com/toronto-on/rentals/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-79.74030661914063%2C%22east%22%3A-79.01246238085938%2C%22south%22%3A43.47534133777822%2C%22north%22%3A43.93992968724773%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A792680%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22mp%22%3A%7B%22max%22%3A2400%7D%2C%22price%22%3A%7B%22max%22%3A459330%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%7D
+
+# create list of {address: x, price: y, link: z} from zillow website using beautiful soup
+# fill out google form that's linked to spreadsheet for each rental in list using selenium
+# email link to spreadsheet to client (myself)
