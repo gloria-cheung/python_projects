fa61847a96a5730c60fb7abde3ab067f1ceb63c0
gloria <gloriacheung812@gmail.com>
2022-11-09 15:42:58 -0500

    create bot that clicks cookie for 5 min to achieve highest clickrate

--- game_playing_bot/main.py
+++ game_playing_bot/main.py
@@ -1,9 +1,54 @@
-from selenium import  webdriver
+import time
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
-service = Service("/Users/macuser/development/chromedriver")
-service.start()
-driver = webdriver.Remote(service.service_url)
+def load_page():
+    chrome_driver_path = Service("/Users/macuser/development/chromedriver")
+    chrome_driver_path.start()
+    chr_options = Options()
+    chr_options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(service=chrome_driver_path, options=chr_options)
+    # open page and wait for popup language selector, close cookie declaration and open stats
+    driver.get("https://orteil.dashnet.org/cookieclicker/")
+    time.sleep(3)
+    driver.find_element(By.ID, "langSelect-EN").click()
+    time.sleep(5)
+    driver.find_element(By.LINK_TEXT, "Got it!").click()
+    driver.find_element(By.ID, "statsButton").click()
+    return driver
 
-driver.get('http://www.amazon.ca/');
+
+def play(driver):
+    cookie = driver.find_element(By.ID, "bigCookie")
+    play_for = 300
+    begin = time.time()
+    # play game for total of 5 min
+    while time.time() - begin < play_for:
+        # every 5 seconds, check what upgrade can be purchased
+        period = 5
+        start = time.time()
+
+        while time.time() - start < period:
+            cookie.click()
+
+        upgrades = driver.find_elements(By.CSS_SELECTOR, ".upgradeBox div.crate")
+        if len(upgrades) > 0:
+            upgrades[0].click()
+
+        store = driver.find_elements(By.CSS_SELECTOR, "#products div.enabled.unlocked")
+        if len(store) > 0:
+            store[len(store) - 1].click()
+
+
+def start():
+    driver = load_page()
+    play(driver)
+
+    time.sleep(5)
+    print(driver.find_element(By.ID, "statsGeneral").find_elements(By.CSS_SELECTOR, "div.listing")[5].text)
+
+
+start()
