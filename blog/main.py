751e0c5b7ccb1d555686b3dd074fdf493a5c2a41
gloria <gloriacheung812@gmail.com>
2022-11-11 13:09:59 -0500

    add functions for blog and each post

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("index.html")
+@app.route('/blog')
+def blog():
+    posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+    return render_template("index.html", posts=posts)
+
+
+@app.route("/post/<int:id>")
+def post(id):
+    posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+    single_post = None
+    for post in posts:
+        if post["id"] == id:
+            single_post = post
+    return render_template("post.html", post=single_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
