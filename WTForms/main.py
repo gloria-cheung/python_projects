35fe8c67a4c61453cf0d9062f995c96d3456f524
gloria <gloriacheung812@gmail.com>
2022-11-14 15:34:42 -0500

    change to display success page when email and password match validators

--- WTForms/main.py
+++ WTForms/main.py
@@ -13,7 +13,10 @@ def home():
 def login():
     form = MyForm()
     if form.validate_on_submit():
-        return redirect('/success')
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template("login.html", form=form)
 
 if __name__ == '__main__':
