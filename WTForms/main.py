20b52ce9d9be18ef99cb2e03337187c6f16664ed
gloria <gloriacheung812@gmail.com>
2022-11-14 15:15:37 -0500

    import form class and instantiate the form and pass to template for login route

--- WTForms/main.py
+++ WTForms/main.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from forms import MyForm
+
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'your secret key'
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template("login.html", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
