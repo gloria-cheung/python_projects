04e0dc321d5037d980c33d57f936480a66cd555f
gloria <gloriacheung812@gmail.com>
2022-11-14 12:08:41 -0500

    fetch data from api endpoint to render posts data

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import requests
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+    return render_template("index.html", posts=posts)
 
 
 @app.route("/about")
