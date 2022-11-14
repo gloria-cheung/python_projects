c081f7e2f4198d1303acf5a1c2675202234c8cd3
gloria <gloriacheung812@gmail.com>
2022-11-14 12:19:07 -0500

    add route for individual post

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -9,6 +9,16 @@ def index():
     return render_template("index.html", posts=posts)
 
 
+@app.route("/post/<int:id>")
+def post(id):
+    posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+    found_post = None
+    for post in posts:
+        if post["id"] == id:
+            found_post = post
+    return render_template("post.html", post=found_post)
+
+
 @app.route("/about")
 def about():
     return render_template("about.html")
