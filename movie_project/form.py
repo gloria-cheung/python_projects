38cd28545bd68914530d4b54f931be8a57e6183f
gloria <gloriacheung812@gmail.com>
2022-11-15 17:44:03 -0500

    create add form to search movie title using api

--- movie_project/form.py
+++ movie_project/form.py
@@ -7,3 +7,8 @@ class EditForm(FlaskForm):
     rating = StringField('Your Rating Out of 10 eg 7.5:', validators=[DataRequired()])
     review = StringField('Your Review:', validators=[DataRequired()])
     submit = SubmitField('Done')
+
+
+class AddForm(FlaskForm):
+    title = StringField('Movie Title:', validators=[DataRequired()])
+    submit = SubmitField('Add Movie')
