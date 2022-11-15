7705136b5dbe86e0d31e650b57b683db6e234c52
gloria <gloriacheung812@gmail.com>
2022-11-15 15:45:03 -0500

    create editform class using wtfform and flask

---
+++ movie_project/form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class EditForm(FlaskForm):
+    rating = StringField('Your Rating Out of 10 eg 7.5:', validators=[DataRequired()])
+    review = StringField('Your Review:', validators=[DataRequired()])
+    submit = SubmitField('Done')
