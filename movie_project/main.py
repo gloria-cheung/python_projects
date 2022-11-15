279738d49474f10d7d3ab881884775a2c5136f61
gloria <gloriacheung812@gmail.com>
2022-11-15 15:05:29 -0500

    import model and query db to get all movies for / route

--- movie_project/main.py
+++ movie_project/main.py
@@ -1,19 +1,26 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
+from model import db, Movie
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+db.init_app(app)
 Bootstrap(app)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    movies = Movie.query.all()
+    return render_template("index.html", movies=movies)
 
 
 if __name__ == '__main__':
