5aa62467b39d68b27ef24a79bbeb73fc34f80f4e
gloria <gloriacheung812@gmail.com>
2022-11-15 10:38:20 -0500

    add home route to render books list and add route to add new book to books list after form completion

--- library/main.py
+++ library/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -7,14 +7,24 @@ all_books = []
 
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        book_dict = {}
+        for item in request.form.items():
+            if item[0] == "rating":
+                book_dict[item[0]] = int(item[1])
+            else:
+                book_dict[item[0]] = item[1]
+        all_books.append(book_dict)
+
+        return redirect("/")
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
+    
