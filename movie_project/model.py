46c4bce66b4e8b561f6804a07afef32a24e24c85
gloria <gloriacheung812@gmail.com>
2022-11-15 15:04:38 -0500

    create movie model

--- movie_project/model.py
+++ movie_project/model.py
@@ -1,4 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String, nullable=False)
+    img_url = db.Column(db.String, nullable=False)
