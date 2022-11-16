e32ad77efe8de37d57773fa8595a5e4d33c60b20
gloria <gloriacheung812@gmail.com>
2022-11-16 18:05:06 -0500

    use flask_login to login user and only allow authenticated user to view secrets page

--- authentication/main.py
+++ authentication/main.py
@@ -10,6 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 ##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
@@ -23,24 +25,49 @@ with app.app_context():
     db.create_all()
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # hash the password before save to db
+        hashed_password = generate_password_hash(request.form["password"], salt_length=8)
+        new_user = User(
+            email=request.form["email"],
+            name=request.form["name"],
+            password=hashed_password
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return render_template("secrets.html", name=new_user.name)
+
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check email and password against user in db -> then login using flask_login
+        user = User.query.filter_by(email=request.form["email"]).first()
+        if check_password_hash(user.password, request.form["password"]):
+            login_user(user)
+        return render_template("secrets.html", name=user.name)
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    return render_template("secrets.html", name=current_user.name)
 
 
 @app.route('/logout')
@@ -50,7 +77,7 @@ def logout():
 
 @app.route('/download')
 def download():
-    pass
+    return send_from_directory("static", "files/cheat_sheet.pdf")
 
 
 if __name__ == "__main__":
