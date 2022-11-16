3fc94ba78c8cbbcf2b1e426862acdc6ab83440c7
gloria <gloriacheung812@gmail.com>
2022-11-16 13:04:32 -0500

    render form to create new post and retrieve data from form to save to db with generated date for now

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap
 from smtplib import SMTP
 from dotenv import load_dotenv
 import os
 from model import BlogPost, db
+from form import CreatePostForm
+from flask_ckeditor import CKEditor
+import datetime
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
 Bootstrap(app)
 db.init_app(app)
 
@@ -30,9 +34,25 @@ def post(id):
     return render_template("post.html", post=found_post)
 
 
-@app.route("/new-post")
+@app.route("/new-post", methods=["GET", "POST"])
 def create():
-    return render_template("make-post.html")
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        date = datetime.datetime.now()
+        formatted_date = f"{date.strftime('%B')} {date.day}, {date.year}"
+
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            author=form.author.data,
+            img_url=form.img_url.data,
+            body=form.body.data,
+            date=formatted_date
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect("/")
+    return render_template("make-post.html", form=form)
 
 
 @app.route("/about")
