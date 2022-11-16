90d66b4b6eecfcd7bc6ccea9022e19dd6b261a5e
gloria <gloriacheung812@gmail.com>
2022-11-16 18:27:08 -0500

    pass logged_in variable to index so that login and register buttons are hidden when user is logged in

--- authentication/main.py
+++ authentication/main.py
@@ -33,7 +33,7 @@ def load_user(user_id):
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=["GET", "POST"])
